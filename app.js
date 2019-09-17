52f819a250d0d2f618c4a9bf54713d37285ac5e4
CN105 <cn105@CN0105.local>
2019-09-17 11:35:28 +0100

    using json parse in the request

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const request = require('request')
 const apiKey = "c626a74f746d221a941c09e743f9c58f"
 
 //  object(conatains the api url etc) , callback funtion (which contain 'error' and 'responce')
-request({url:"https://api.darksky.net/forecast/c626a74f746d221a941c09e743f9c58f/37.8267,-122.4233"}, (error, responce) => {
+request({url:"https://api.darksky.net/forecast/c626a74f746d221a941c09e743f9c58f/37.8267,-122.4233", json: true}, (error, responce) => {
     if(error){
         console.log(error)
     }
     else {
         // console.log(responce)
-        // need tp parse the JSON data
-        const data = JSON.parse(responce.body)
-        console.log(data.currently.temperature) //  for temperature .currently.temperature
+        // need to parse the JSON data
+
+        // const data = JSON.parse(responce.body)   added JSON: true to 'request' which dose this for us 
+        const data = responce.body   
+
+        // console.log(`  temp is --> ${data.currently.temperature}`) //  for temperature .currently.temperature
+
+        console.log({temp: data.currently.temperature, rain: data.currently.precipProbability})
     }
 })
